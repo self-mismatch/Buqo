668971382280b2280cbecb7b0b88f39b988fee41
Vladislav <self.mismatch@gmail.com>
2022-04-23 23:05:20 +0300

    feat: Add Firebase and Taiga UI to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'buqo-c95d0',
+    appId: '1:934896127530:web:f3f9cbcf43812ccc507d53',
+    storageBucket: 'buqo-c95d0.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCZNtDFb5ahZwmwsb_D86ETWsYk053R_NY',
+    authDomain: 'buqo-c95d0.firebaseapp.com',
+    messagingSenderId: '934896127530',
+    measurementId: 'G-LT3T44KY5J',
+  },
   production: false
 };
 
