7fa95e386d34b0b31dc85707524167f665d67792
Vladislav <self.mismatch@gmail.com>
2022-04-23 23:49:42 +0300

    feat: Lint project and add one rule for default export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER,
+} from '@taiga-ui/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,12 +23,12 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-      TuiRootModule,
-      BrowserAnimationsModule,
-      TuiDialogModule,
-      TuiNotificationsModule
-],
-  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
-  bootstrap: [AppComponent]
+    TuiRootModule,
+    BrowserAnimationsModule,
+    TuiDialogModule,
+    TuiNotificationsModule,
+  ],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
