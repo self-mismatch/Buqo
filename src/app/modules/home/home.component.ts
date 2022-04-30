67f7273fa44feb1ea6d96eb016cc370dc38a8406
Vladislav <self.mismatch@gmail.com>
2022-04-30 23:57:04 +0300

    feat: add posts on home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Post } from '../../core/interfaces/post';
+import { PostsService } from '../../core/services/posts/posts.service';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +11,16 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  posts: Observable<Post[]> = this.postsService.getAllPosts();
+
+  constructor(private postsService: PostsService, private router: Router) {}
+
+  openPost(post: Post): void {
+    this.router.navigate(['/post'], {
+      queryParams: {
+        id: post.id,
+      },
+    });
+  }
+}
